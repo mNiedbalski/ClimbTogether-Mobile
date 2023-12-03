cb8d4550887bab75b0ceaf95483f66a19717abb6
Michał Niedbalski <michnie507@student.polsl.pl>
2023-12-03 15:16:23 +0100

    Dodawanie

--- src/components/RecordClimbing/RecordClimbing.js
+++ src/components/RecordClimbing/RecordClimbing.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NativeBaseProvider, Box, Text, Button } from 'native-base';
+import { Attempt } from '../../Entities/attempt';
+import { v4 as uuidv4 } from 'uuid';
 
 const loadRouteParams = async (route) => {
   return new Promise((resolve) => {
@@ -14,6 +16,7 @@ const loadRouteParams = async (route) => {
 };
 
 const RecordClimbing = ({ route }) => {
+  const [attempt, setAttempt] = useState({});
   const [selectedRoute, setSelectedRoute] = useState({});
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerStopped, setTimerStopped] = useState(false);
@@ -26,6 +29,18 @@ const RecordClimbing = ({ route }) => {
     const millisecondsPart = milliseconds % 1000;
     return `${seconds}.${millisecondsPart < 100 ? '0' : ''}${millisecondsPart}`;
   };
+  const createNewAttempt = (measuredTime, wasZoneReached, wasTopReached) => {
+    const newAttempt = new Attempt(uuidv4(), new Date(), measuredTime, wasZoneReached, wasTopReached);
+    setAttempt(newAttempt);
+    updateRouteAttempts(newAttempt);
+  };
+  const updateRouteAttempts = (newAttempt) => {
+    console.log("AKTUALNA PROBA",newAttempt);
+    setSelectedRoute(prevRoute => {
+      const updatedAttempts = [...prevRoute.attempts, newAttempt];
+      return { ...prevRoute, attempts: updatedAttempts };
+    });
+  }
 
   const startTimer = () => {
     setTimerStarted(true);
@@ -33,11 +48,11 @@ const RecordClimbing = ({ route }) => {
     setZoneReached(false);
     setTopReached(false);
     setElapsedTime(0);
-    console.log(selectedRoute);
   };
 
   const stopTimer = () => {
     setTimerStopped(true);
+    createNewAttempt(elapsedTime, zoneReached, topReached);
   };
 
   const handleZoneReached = () => {
@@ -45,14 +60,13 @@ const RecordClimbing = ({ route }) => {
   };
 
   const handleTopReached = () => {
-    setTopReached(true);
     setTimerStopped(true);
+    createNewAttempt(elapsedTime, true, true);
   };
-
   const restartRecording = () => {
     setTimerStarted(false);
     setTimerStopped(false);
-    setZoneReached(false);
+    setZoneReached(false)
     setTopReached(false);
     setElapsedTime(0);
   };
@@ -61,10 +75,25 @@ const RecordClimbing = ({ route }) => {
     const fetchData = async () => {
       const loadedRoute = await loadRouteParams(route);
       setSelectedRoute(loadedRoute);
+
     };
 
     fetchData();
   }, [route]);
+  useEffect(() => {
+    console.log("routes.attempts updated:", selectedRoute.attempts);
+  }, [selectedRoute]);
+  useEffect(() => {
+    console.log("zoneReached updated:", zoneReached);
+  }, [zoneReached]);
+
+  useEffect(() => {
+    console.log("topReached updated:", topReached);
+  }, [topReached]);
+  useEffect(() => {
+    console.log("attempt updated:", attempt);
+  }, [attempt]);
+
 
   useEffect(() => {
     let timeout;
@@ -102,6 +131,7 @@ const RecordClimbing = ({ route }) => {
         {topReached && (
           <>
             <Text>Elapsed Time: {formatTime(elapsedTime)} s</Text>
+            <Text>Parameters of attempt: {console.log(attempt)}</Text>
             <Button onPress={restartRecording}>Retry</Button>
             <Button>Go back</Button>
           </>
