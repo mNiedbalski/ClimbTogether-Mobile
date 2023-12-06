1763986c74c365a407c150832a0d1fb4043cecd9
Michał Niedbalski <michnie507@student.polsl.pl>
2023-12-06 02:32:35 +0100

    Modified entities due to lack of logic sometimes

--- src/Entities/route.ts
+++ src/Entities/route.ts
@@ -1,25 +1,22 @@
 import { Attempt } from "./attempt";
-import { Routesetter } from "./routesetter";
+import { User } from "./user";
 export class Route {
   id: number;
   route_name: string;
-  routesetter: Routesetter;
+  routesetter: User;
   difficulty: string;
   attempts: Attempt[];
-  best_attempt: Attempt;
   constructor(
     id: number,
     route_name: string,
-    routesetter: Routesetter,
+    routesetter: User,
     difficulty: string,
     attempts: Attempt[],
-    best_attempt: Attempt,
   ) {
     this.id = id;
     this.route_name = route_name;
     this.routesetter = routesetter;
     this.difficulty = difficulty;
     this.attempts = attempts;
-    this.best_attempt = best_attempt;
   }
 }
